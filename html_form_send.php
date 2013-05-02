e039f6cc4beb685e53301227371c013c3644061f
Christopher Haverman <phasetwenty@gmail.com>
2013-05-01 23:37:32 -0700

    Initial project state.

---
+++ html_form_send.php
@@ -0,0 +1,149 @@
+<?php
+$fields = array('lastname' => 'Last Name',
+    'firstname' => 'First Name',
+    'MI' => 'Middle Initial',
+    'street_address' => 'Address',
+    'city' => 'City',
+    'State' => 'State',
+    'zip' => 'ZIP',
+    'DL' => 'Driver License #',
+    'Phone' => 'Phone',
+    'email' => 'Email',
+    'date' => 'Date Available',
+    'salary' => 'Desired Salary',
+    'position' => 'Positions Applied',
+    'us_citizen' => 'Are you a citizen',
+    'Authorized' => 'Are you authorized to work',
+    'employed_prior' => 'Have you ever worked for this company',
+    'When' => 'If yes, when',
+    'felony' => 'Have you ever been convicted of a felony',
+    'felony_explain' => 'If yes, explain',
+    'high_school' => 'High school name',
+    'HS_Address' => 'Address',
+    'Year_Start' => 'Started',
+    'Year_End' => 'Completed',
+    'graduate_HS' => 'Did you graduate',
+    'Diploma' => 'Diploma',
+    'College' => 'College name',
+    'College_Address' => 'Address',
+    'Year_Start2' => 'Started',
+    'Year_End2' => 'Completed',
+    'graduate_college' => 'Did you graduate',
+    'degree_1' => 'Degree',
+    'Other_Education' => 'Other Education',
+    'other_address' => 'Address',
+    'Year_Start_o' => 'Started',
+    'Year_End_o' => 'Completed',
+    'graduate_other' => 'Did you graduate',
+    'degree_2' => 'Degree',
+    'reference_name_1' => 'Reference 1 name',
+    'reference_relationship_1' => 'Reference 1 relationship',
+    'reference_company_1' => 'Reference 1 company',
+    'reference_phone_1' => 'Reference 1 phone',
+    'reference_address_1' => 'Reference 1 address',
+    'reference_name_2' => 'Reference 2 name',
+    'reference_relationship_2' => 'Reference 2 relationship',
+    'reference_company_2' => 'Reference 2 company',
+    'reference_phone_2' => 'Reference 2 phone',
+    'reference_address_2' => 'Reference 2 address',
+    'reference_name_3' => 'Reference 3 name',
+    'reference_relationship_3' => 'Reference 3 relationship',
+    'reference_company_3' => 'Reference 3 company',
+    'reference_phone_3' => 'Reference 3 phone',
+    'reference_address_3' => 'Reference 3 address',
+    'prev_employ_company_1' => 'Previous company 1 name',
+    'prev_employ_phone_1' => 'Previous company 1 phone',
+    'prev_employ_address_1' => 'Previous company 1 address',
+    'prev_employ_Supervisor_1' => 'Previous company 1 supervisor',
+    'prev_employ_job_title_1' => 'Previous company 1 job title',
+    'prev_employ_start_salary_1' => 'Previous company 1 starting salary',
+    'prev_employ_end_salary_1' => 'Previous company 1 ending salary',
+    'prev_employ_responsibilities_1' => 'Previous company 1 responsibilities',
+    'Month_Start_1' => 'Previous company 1 starting month',
+    'Year_Start_1' => 'Previous company 1 starting year',
+    'Month_End_1' => 'Previous company 1 ending month',
+    'Year_End_1' => 'Previous company 1 ending year',
+    'prev_employ_reason_for_leaving_1' => 'Previous company 1 reason for leaving',
+    'prev_employ_contact_permission_1' => 'Previous company 1 premission to contact',
+    'prev_employ_company_2' => 'Previous company 2 name',
+    'prev_employ_phone_2' => 'Previous company 2 phone',
+    'prev_employ_address_2' => 'Previous company 2 address',
+    'prev_employ_Supervisor_2' => 'Previous company 2 supervisor',
+    'prev_employ_job_title_2' => 'Previous company 2 job title',
+    'prev_employ_start_salary_2' => 'Previous company 2 starting salary',
+    'prev_employ_end_salary_2' => 'Previous company 2 ending salary',
+    'prev_employ_responsibilities_2' => 'Previous company 2 responsibilities',
+    'Month_Start_2' => 'Previous company 2 starting month',
+    'Year_Start_2' => 'Previous company 2 starting year',
+    'Month_End_2' => 'Previous company 2 ending month',
+    'Year_End_2' => 'Previous company 2 ending year',
+    'prev_employ_reason_for_leaving_2' => 'Previous company 2 reason for leaving',
+    'prev_employ_contact_permission_2' => 'Previous company 2 premission to contact',
+    'prev_employ_job_title_2' => 'Previous company 2 job title',
+    'prev_employ_company_3' => 'Previous company 3 name',
+    'prev_employ_phone_3' => 'Previous company 3 phone',
+    'prev_employ_address_3' => 'Previous company 3 address',
+    'prev_employ_Supervisor_3' => 'Previous company 3 supervisor',
+    'prev_employ_job_title_3' => 'Previous company 3 job title',
+    'prev_employ_start_salary_3' => 'Previous company 3 starting salary',
+    'prev_employ_end_salary_3' => 'Previous company 3 ending salary',
+    'prev_employ_responsibilities_3' => 'Previous company 3 responsibilities',
+    'Month_Start_3' => 'Previous company 3 starting month',
+    'Year_Start_3' => 'Previous company 3 starting year',
+    'Month_End_3' => 'Previous company 3 ending month',
+    'Year_End_3' => 'Previous company 3 ending year',
+    'prev_employ_reason_for_leaving_3' => 'Previous company 3 reason for leaving',
+    'military_branch' => 'Military branch',
+    'Month_Start_Military' => 'Military starting month',
+    'Year_Start_Military' => 'Military starting year',
+    'Month_End_Military' => 'Military ending month',
+    'Year_End_Military' => 'Military ending year',
+    'military_rank' => 'Military rank',
+    'military_discharge_type' => 'Military discharge type',
+    'miliary_discharge_explain' => 'Military discharge reason',
+    'menus' => 'Are you able to memorize menus',
+    'shift' => 'Are you able to work an 8 hour shift',
+    'stand' => 'Are you able to stand for long periods',
+    '50lbs' => 'Are you able to lift up to 50 lbs',
+    'weekends_holidays' => 'Are you available to work weekends',
+    'late_nights' => 'Are you able to work late nights',
+    'tray' => 'Can you carry a food or beverage tray',
+    'attitude' => 'Are you able to maintain cheerful, professional attitute',
+    'travel' => 'Are you able to travel',
+    'license' => 'Do you have a license',
+    'transportation' => 'Do you have reliable transportation',
+    'friend' => 'Do you currently have a friend or relative working for Temecula Catering',
+    'emailaccess' => 'Do you have DAILY access to email',
+    'phonereliable' => 'Do you have a reliable cell phone',
+    'drugs' => 'Will you submit to drug screening',
+    'signature' => 'Signature',
+    'signature_Date' => 'Signature date');
+
+$required = array('lastname', 'firstname', 'Phone', 'signature', 'signature_Date');
+
+function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+}
+
+$email_body = "";
+foreach ($fields as $form_name => $form_prompt) {
+    $value = clean_string($_POST[$form_name]);
+    if (isset($_POST[$form_name]) && !empty($value)) {
+        $email_body .= $form_prompt . " \"" . $value . "\"<br />";
+    }
+}
+
+$email_to = "erin@temecula-catering.com"; 
+$email_from = 'admin@temecula-catering.com';
+$email_subject = "Application from Temecula-Catering.com";
+$headers = 'From: '.$email_from."\r\n" . 'Reply-To: '.$email_from."\r\n" . 'X-Mailer: PHP/' . phpversion();
+
+$result = mail($email_to, $email_subject, $email_body, $headers);
+
+
+
+echo $result;
+?>
+
+<p style="font-family:calibri, arial, verdana, tahoma, sans-serif;color:#222;line-height:150%;">Thank you for submitting your application. We will be in touch with you within the next 5-7 business days. In the meantime, feel free to <a href="http://temecula-catering.com/">learn more about our company.</a></p>
